5f5ff114d533908b974f055dde8edf3d0d365bd0
akagamina <omer@gami.gg>
2024-06-05 15:34:19 +0300

    refactor: Update copy button ID to "copySecondBtn" for consistency

--- js/main.js
+++ js/main.js
@@ -205,6 +205,18 @@ preloader
       copySuccess.style.opacity = "0";
     }, 500);
   });
+  $("#copySecondBtn").on("click", function () {
+    let copyText = document.getElementById("copyClipboard");
+    let copySuccess = document.getElementById("copied-success");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+
+    copySuccess.style.opacity = "1";
+    setTimeout(function () {
+      copySuccess.style.opacity = "0";
+    }, 500);
+  });
 
   function submitMSG(valid, msg) {
     if (valid) {
